a1f99c4b25af265a334f82097b1b35a972feb0b1
win7 <737141437@qq.com>
2018-04-28 11:12:05 +0800

    修复ok，提交

--- app/src/main/java/com/shiming/performanceoptimization/MainActivity.java
+++ app/src/main/java/com/shiming/performanceoptimization/MainActivity.java
@@ -1,13 +1,36 @@
 package com.shiming.performanceoptimization;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+/**
+ * author： Created by shiming on 2018/4/28 10:52
+ * mailbox：lamshiming@sina.com
+ */
+
+public  class MainActivity extends AppCompatActivity {
+
+    private Button mBtn_codeOptimization;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mBtn_codeOptimization = findViewById(R.id.btn_1);
+//        Button viewById = findViewById(R.id.btn_2);
+//        Button viewById = findViewById(R.id.btn_3);
+//        Button viewById = findViewById(R.id.btn_4);
+//        Button viewById = findViewById(R.id.btn_5);
+       mBtn_codeOptimization.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               startActivity(new Intent(MainActivity.this,CodeOptimizationActivity.class));
+           }
+       });
     }
 }
