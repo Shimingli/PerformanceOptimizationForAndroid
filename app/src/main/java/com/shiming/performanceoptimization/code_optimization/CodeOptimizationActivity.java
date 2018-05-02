c94dacc480eb7eb5f4aeba148791771733307623
win7 <737141437@qq.com>
2018-05-02 16:26:17 +0800

    提交写代码的注意的事项

--- app/src/main/java/com/shiming/performanceoptimization/code_optimization/CodeOptimizationActivity.java
+++ app/src/main/java/com/shiming/performanceoptimization/code_optimization/CodeOptimizationActivity.java
@@ -117,12 +117,37 @@ public  class CodeOptimizationActivity extends AppCompatActivity {
                 startActivity(new Intent(CodeOptimizationActivity.this,HandlerActivity.class));
             }
         });
+        //java 四种引用方式和引用队列的解释
         findViewById(R.id.btn_four_ways_of_reference_demo).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(CodeOptimizationActivity.this,JavaFourWaysOfReferenceActivity.class));
             }
         });
+        //1、不要重复的创建相同的对象，对象的创建都是需要内存分配的，对象的销毁需要垃圾回收，这些都在一定程度上影响程序的性能
+
+        //2、对常量使用static final修饰，对于基本类型和String类型的常量，建议使用常量static final 修饰，因为final类型的常量会在静态dex文件的域初始化部分，这时对基本数据类型和String类型常量的调用不会涉及类的初始化，而是直接调用字面量
+
+        /**
+         * JIT是”Just In Time Compiler”的缩写，就是”即时编译技术”，与Dalvik虚拟机相关,JIT是在2.2版本提出的，目的是为了提高Android的运行速度，一直存活到4.4版本，因为在4.4之后的ROM中，就不存在Dalvik虚拟机了。
+         * 编译打包APK文件:1、Java编译器将应用中所有Java文件编译为class文件,2、dx工具将应用编译输出的类文件转换为Dalvik字节码，即dex文件
+         * Google在2.2版本添加了JIT编译器，当App运行时，每当遇到一个新类，JIT编译器就会对这个类进行编译，经过编译后的代码，会被优化成相当精简的原生型指令码（即native code），这样在下次执行到相同逻辑的时候，速度就会更快。
+         * dex字节码翻译成本地机器码是发生在应用程序的运行过程中的，并且应用程序每一次重新运行的时候，都要做重做这个翻译工作，所以这个工作并不是一劳永逸，每次重新打开App，都需要JIT编译，Dalvik虚拟机从Android一出生一直活到4.4版本，而JIT在Android刚发布的时候并不存在，在2.2之后才被添加到Dalvik中。
+         * AOT是”Ahead Of Time”的缩写，指的就是ART(Anroid RunTime)这种运行方式。
+         * JIT是运行时编译，这样可以对执行次数频繁的dex代码进行编译和优化，减少以后使用时的翻译时间，虽然可以加快Dalvik运行速度，但是还是有弊病，那就是将dex翻译为本地机器码也要占用时间，所以Google在4.4之后推出了ART，用来替换Dalvik。
+         * ART的策略与Dalvik不同，在ART 环境中，应用在第一次安装的时候，字节码就会预先编译成机器码，使其成为真正的本地应用。之后打开App的时候，不需要额外的翻译工作，直接使用本地机器码运行，因此运行速度提高。
+         *
+         * 当然ART与Dalvik相比，还是有缺点的。
+         *
+         * ART需要应用程序在安装时，就把程序代码转换成机器语言，所以这会消耗掉更多的存储空间，但消耗掉空间的增幅通常不会超过应用代码包大小的20%
+         * 由于有了一个转码的过程，所以应用安装时间难免会延长
+         *
+         * 但是这些与更流畅的Android体验相比而言，不值一提。
+         */
+        //3、避免内部的get set方法的调用，get set的作用是对以外屏蔽具体的变量定义，从而达到更好的封装性，如果在类的内部调用get set的方法访问变量的话，会降低访问的速度，根据在安卓的官方的文档，在没有jit编译器时，直接访问变量的速度是调用get方法的3倍，在jit编译器，直接访问变量是调用get方法的7倍，当然使用了ProGuard的话，perGuard会对get set 进行内联的操作，从而达到直接访问的效果，
+
+
+
     }
 
     /**
